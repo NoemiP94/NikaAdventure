d8071a2850ac4c2a3fae1e628da7f1e810ccc477
NoemiP94 <m_emi94@hotmail.it>
2024-09-10 10:41:36 +0200

    monster projectiles

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -116,16 +116,7 @@ public class Entity { //abstract class
         boolean contactPlayer = gp.cChecker.checkPlayer(this);
 
         if(this.type == type_monster && contactPlayer == true){
-            if(gp.player.invincible == false){
-                //we can give damage
-                gp.playSE(6);
-                int damage = attack - gp.player.defense;
-                if(damage <= 0){
-                    damage = 0;
-                }
-                gp.player.life -= damage;
-                gp.player.invincible = true;
-            }
+            damagePlayer(attack);
         }
 
         //movement
@@ -164,6 +155,21 @@ public class Entity { //abstract class
                 invincible = false;
                 invincibleCounter = 0;
             }
+        }
+        if(shotAvailableCounter < 30){
+            shotAvailableCounter++;
+        }
+    }
+    public void damagePlayer(int attack){
+        if(gp.player.invincible == false){
+            //we can give damage
+            gp.playSE(6);
+            int damage = attack - gp.player.defense;
+            if(damage <= 0){
+                damage = 0;
+            }
+            gp.player.life -= damage;
+            gp.player.invincible = true;
         }
     }
 
